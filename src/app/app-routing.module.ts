1450131b694a528541b759e70cb9163b117cb9a3
kishino kango <kishinokango@kishinonoMacBook-Pro.local>
2022-03-24 11:21:02 +0900

    create component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ParentComponent } from './parent/parent.component';
+import { ChildAComponent } from './parent/child-a/child-a.component';
+import { ChildBComponent } from './parent/child-b/child-b.component';
+import { ChildCComponent } from './parent/child-c/child-c.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'parent', component: ParentComponent },
+  { path: 'parent/child-a', component: ChildAComponent },
+  { path: 'parent/child-b', component: ChildBComponent },
+  { path: 'parent/child-c', component: ChildCComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
